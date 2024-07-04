6223709c979e5758ec9570420ff731cd47f68c61
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-04 20:13:27 +0500

    eighth  Assignment

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Assignment 08
+// creat lines look like this console.log(5+3)
+console.log(12 / 6);
+console.log(7 * 9);
+console.log(8 + 9);
+console.log(9 - 4);
---
+++ main.ts
@@ -0,0 +1,20 @@
+//Assignment 08
+
+// creat lines look like this console.log(5+3)
+
+console.log(12/6)
+
+console.log(7*9)
+
+console.log(8+9)
+
+console.log(9-4)
+
+
+
+
+
+
+
+
+
